50055cce3cb69df12c6fbac5f7b880ff7980d313
Joshuajrodrigues <54748294+Joshuajrodrigues@users.noreply.github.com>
2019-09-01 00:11:47 +0530

    Add files via upload

---
+++ madLibs.py
@@ -0,0 +1,10 @@
+print('MAD LIBS'.center(50))
+print('Lets get started !')
+
+name= input('Give me a single name ')
+color = input(' Give me a single color ')
+animal = input(' Your fav animal ')
+
+print(str(name)+' is a '+str(color)+' faced '+str(animal) )
+
+print(' DISCLAIMER : THIS IS JUST A JOKE, I COULDNT COME UP WITH ANYTHING ELSE AND I NEVER PLAYED MAD LIBS BEFORE')
